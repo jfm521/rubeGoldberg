d8d4d0e644229d0f9fcc41f3b77f1e8d3c471bee
Jackie Murray <jfm521@nyu.edu>
2021-03-01 00:31:21 -0500

    final build(?)

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -6,8 +6,12 @@ public class CameraBehavior : MonoBehaviour
 {
 
     public Transform followTransform;
+    public Transform followTransform1;
+    public Transform followTransform2;
     public BoxCollider2D worldbounds;
 
+    public float ball1y;
+    public float ball2y;
     float xMin;
     float xMax;
     float yMin;
@@ -28,10 +32,15 @@ public class CameraBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        xMin = worldbounds.bounds.min.x;
-        xMax = worldbounds.bounds.max.x;
-        yMin = worldbounds.bounds.min.y;
-        yMax = worldbounds.bounds.max.y;
+        //xMin = worldbounds.bounds.min.x;
+        //xMax = worldbounds.bounds.max.x;
+        //yMin = worldbounds.bounds.min.y;
+        //yMax = worldbounds.bounds.max.y;
+
+        xMin = -100;
+        xMax = 100;
+        yMin = -100;
+        yMax = 100;
 
         mainCam = gameObject.GetComponent<Camera>();
 
@@ -42,11 +51,26 @@ public class CameraBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        camY = Mathf.Clamp(followTransform.position.y, yMin + camSize, yMax - camSize);
-        camX = Mathf.Clamp(followTransform.position.x, xMin + camSize, xMax - camSize);
+        if (followTransform2.position.y <= ball2y)
+        {
+            camY = Mathf.Clamp(followTransform2.position.y, yMin + camSize, yMax - camSize);
+            camX = Mathf.Clamp(followTransform2.position.x, xMin + camSize, xMax - camSize);
+        }
+        else if (followTransform1.position.y <= ball1y)
+        {
+            camY = Mathf.Clamp(followTransform1.position.y, yMin + camSize, yMax - camSize);
+            camX = Mathf.Clamp(followTransform1.position.x, xMin + camSize, xMax - camSize);
+        }
+        else
+        {
+            camY = Mathf.Clamp(followTransform.position.y, yMin + camSize, yMax - camSize);
+            camX = Mathf.Clamp(followTransform.position.x, xMin + camSize, xMax - camSize);
+        }
+
 
         smoothPos = Vector3.Lerp(gameObject.transform.position, new Vector3(camX, camY, gameObject.transform.position.z), smoothRate);
 
         gameObject.transform.position = smoothPos;
+
     }
 }
