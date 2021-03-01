3db8ba8c9735ccafd3e432122528326ecd92a17b
Jackie Murray <jfm521@nyu.edu>
2021-02-28 20:07:57 -0500

    initial commit

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+
+    Rigidbody2D myBody;
+
+    public float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myBody.AddForce(new Vector3(10, 0, 0) * power);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.name == "Floor")
+        {
+            Debug.Log("Hit something");
+        }
+    }
+}
---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+
+    public Transform followTransform;
+    public BoxCollider2D worldbounds;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    float camX;
+    float camY;
+
+    float camRatio;
+    float camSize;
+
+    Camera mainCam;
+
+    Vector3 smoothPos;
+
+    public float smoothRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMin = worldbounds.bounds.min.x;
+        xMax = worldbounds.bounds.max.x;
+        yMin = worldbounds.bounds.min.y;
+        yMax = worldbounds.bounds.max.y;
+
+        mainCam = gameObject.GetComponent<Camera>();
+
+        camSize = mainCam.orthographicSize;
+        camRatio = (xMax + camSize) / 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camY = Mathf.Clamp(followTransform.position.y, yMin + camSize, yMax - camSize);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + camSize, xMax - camSize);
+
+        smoothPos = Vector3.Lerp(gameObject.transform.position, new Vector3(camX, camY, gameObject.transform.position.z), smoothRate);
+
+        gameObject.transform.position = smoothPos;
+    }
+}
